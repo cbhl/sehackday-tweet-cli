f443f5c6e2a14f865aec1d5be3bf1592d275ae1a
Michael Chang <thenewme91@gmail.com>
2013-10-26 19:48:18 -0400

    Initialize ncurses.

--- sehackday-tweet-cli.go
+++ sehackday-tweet-cli.go
@@ -11,5 +11,15 @@ import (
 )
 
 func main() {
+    // Initialize the screen.
+    // WARNING: ncurses is not thread-safe.
+    // TODO(cbhl): isolate ncurses code into a GUI goroutine
+    _, err := goncurses.Init()
+    if err != nil {
+        log.Fatal("init:", err)
+        os.Exit(1)
+    }
+    defer goncurses.End() // Clean up when we're done.
+
     fmt.Printf("Hello, world.\n")
 }
