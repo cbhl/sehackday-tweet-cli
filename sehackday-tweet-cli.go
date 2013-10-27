ae1782e5e8f47f307271d6c8ec53e7820d1574f0
Michael Chang <thenewme91@gmail.com>
2013-10-26 20:28:21 -0400

    Print a title.

--- sehackday-tweet-cli.go
+++ sehackday-tweet-cli.go
@@ -5,7 +5,7 @@ import (
 //    "github.com/mattn/go-sqlite3"
 //    "github.com/araddon/httpstream"
 //    "database/sql"
-    "fmt"
+//    "fmt"
     "log"
     "os"
 )
@@ -14,12 +14,19 @@ func main() {
     // Initialize the screen.
     // WARNING: ncurses is not thread-safe.
     // TODO(cbhl): isolate ncurses code into a GUI goroutine
-    _, err := goncurses.Init()
+    scr, err := goncurses.Init()
     if err != nil {
         log.Fatal("init:", err)
         os.Exit(1)
     }
     defer goncurses.End() // Clean up when we're done.
 
-    fmt.Printf("Hello, world.\n")
+    scr.Keypad(true)
+    _, width := scr.Maxyx()
+    title := "#sehackday"
+    scr.MovePrint(0, (width - len(title))/2, title)
+    scr.Move(1,0)
+    scr.Print("Hello, world.")
+    scr.Refresh()
+    scr.GetChar()
 }
